5e63732b5506bd0207cb5ce2f69a1eaae670e653
babarictypos <72893835+babarictypos@users.noreply.github.com>
2022-07-04 10:54:59 +0100

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,11 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/4bhb07fv3s4pic5/export.pkl?raw=1'
+#export_file_url = 'https://www.dropbox.com/s/4bhb07fv3s4pic5/export.pkl?raw=1'
+export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
 export_file_name = 'export.pkl'
 
-classes = ['solved', 'scrambled', 'not found']
+classes = ['solved', 'scrambled', 'not_found']
 path = Path(__file__).parent
 
 app = Starlette()
